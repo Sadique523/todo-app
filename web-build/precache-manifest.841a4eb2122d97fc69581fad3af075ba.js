6609efc79da5104bc179d1593c7c30d2725151c9
Sadique523 <sadique@IOs-iMac.local>
2020-04-26 04:45:38 +0530

    web-build

---
+++ web-build/precache-manifest.841a4eb2122d97fc69581fad3af075ba.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba290559496e304a7110",
+    "url": "/static/js/app.6d25f114.chunk.js"
+  },
+  {
+    "revision": "12285de5044dece6d261",
+    "url": "/static/js/runtime~app.a6639e49.js"
+  },
+  {
+    "revision": "942f828ceaf99827657b",
+    "url": "/static/js/2.0e9c369a.chunk.js"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/./fonts/AntDesign.ttf"
+  },
+  {
+    "revision": "5a293a273bee8d740a045d9922b9a9ae",
+    "url": "/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "1eef72823743d9c47d8840e55b0823f0",
+    "url": "/static/media/avatar.1eef7282.png"
+  },
+  {
+    "revision": "e1437cc0de0b01787ba656236aaaa075",
+    "url": "/./fonts/RubikMonoOne-Regular.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/expo-service-worker.js"
+  },
+  {
+    "revision": "afd5733ac789ab1795f1ed7a26e1ecd3",
+    "url": "/favicon-16.png"
+  },
+  {
+    "revision": "3d85359b37ca7590f8e91d593d02ddaf",
+    "url": "/favicon-32.png"
+  },
+  {
+    "revision": "5e52768b0f7cbb62cbe07751add33101",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "6b5cf85ac75625814c57c60e5ccc79b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6337d4a07cb942e2e953bd2c57db5d8",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "04cf1ae10e30155ca207400239ec1cbd",
+    "url": "/register-service-worker.js"
+  }
+];
